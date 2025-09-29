4868d06b6d660626d6224ee2e527e4545c78431d
Nikolay-Yakovlev <61546519+Nikolay-Yakovlev@users.noreply.github.com>
2025-09-29 17:07:52 +0300

    t3

--- FilesHelper.cs
+++ FilesHelper.cs
@@ -13,9 +13,6 @@ namespace HTML_to_PDF_converter
         {
             try
             {
-                //test
-                //test
-                //test
                 var convertedFileList = new List<ConvertedFile>();
                 var foldersInfo = new DirectoryInfo(path).GetDirectories();
                 foreach (var fld in foldersInfo)
